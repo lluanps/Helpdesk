71f374c2942e4b89fe66f7a431b99c657b585375
lluanps <lluanps@gmail.com>
2023-06-24 14:08:10 -0300

    Perfil de test criado

--- helpdesk/src/main/java/com/luan/helpdesk/HelpdeskApplication.java
+++ helpdesk/src/main/java/com/luan/helpdesk/HelpdeskApplication.java
@@ -1,50 +1,13 @@
 package com.luan.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.luan.helpdesk.domain.Chamado;
-import com.luan.helpdesk.domain.Cliente;
-import com.luan.helpdesk.domain.Instrutor;
-import com.luan.helpdesk.domain.enums.Perfil;
-import com.luan.helpdesk.domain.enums.Prioridade;
-import com.luan.helpdesk.domain.enums.Status;
-import com.luan.helpdesk.repositories.ChamadoRepository;
-import com.luan.helpdesk.repositories.ClienteRepository;
-import com.luan.helpdesk.repositories.InstrutorRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
+public class HelpdeskApplication {
 
-	@Autowired
-	private InstrutorRepository instrutorRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private ChamadoRepository chamadoRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Instrutor inst1 = new Instrutor(null, "Luan Pinheiro", "222.632.070-90", "luan@gmail.com", "admin");
-		inst1.addPerfil(Perfil.ADMIN);
-		
-		Cliente cli1 = new Cliente(null, "Messi", "530.212.030-51", "messi@gmail.com", "123");
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", inst1, cli1);
-		
-		instrutorRepository.saveAll(Arrays.asList(inst1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
---
+++ helpdesk/src/main/java/com/luan/helpdesk/services/DBService.java
@@ -0,0 +1,45 @@
+package com.luan.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luan.helpdesk.domain.Chamado;
+import com.luan.helpdesk.domain.Cliente;
+import com.luan.helpdesk.domain.Instrutor;
+import com.luan.helpdesk.domain.enums.Perfil;
+import com.luan.helpdesk.domain.enums.Prioridade;
+import com.luan.helpdesk.domain.enums.Status;
+import com.luan.helpdesk.repositories.ChamadoRepository;
+import com.luan.helpdesk.repositories.ClienteRepository;
+import com.luan.helpdesk.repositories.InstrutorRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private InstrutorRepository instrutorRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;	
+
+	public void instanciaDB() {
+		
+		Instrutor inst1 = new Instrutor(null, "Luan Pinheiro", "222.632.070-90", "luan@gmail.com", "admin");
+		inst1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Messi", "530.212.030-51", "messi@gmail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", inst1, cli1);
+		
+		instrutorRepository.saveAll(Arrays.asList(inst1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+	
+	
+}
