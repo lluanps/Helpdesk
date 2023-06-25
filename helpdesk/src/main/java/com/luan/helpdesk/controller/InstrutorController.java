086f66cbe76e7ac689bd888d1a09aa1a02a21939
lluanps <lluanps@gmail.com>
2023-06-24 21:23:05 -0300

    Exceção para cpf e email ja cadastrados

--- helpdesk/src/main/java/com/luan/helpdesk/controller/InstrutorController.java
+++ helpdesk/src/main/java/com/luan/helpdesk/controller/InstrutorController.java
@@ -2,6 +2,7 @@ package com.luan.helpdesk.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luan.helpdesk.domain.Instrutor;
+import com.luan.helpdesk.domain.Pessoa;
 import com.luan.helpdesk.domain.dtos.InstrutorDTO;
+import com.luan.helpdesk.repositories.PessoaRepository;
 import com.luan.helpdesk.services.InstrutorService;
+import com.luan.helpdesk.services.exceptions.DataIntegrityViolationException;
 
 @RestController
 @RequestMapping(value = "/instrutores")
@@ -24,6 +28,9 @@ public class InstrutorController {
 	
 	@Autowired
 	private InstrutorService service;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<InstrutorDTO> findById(@PathVariable Integer id) {
@@ -41,8 +48,22 @@ public class InstrutorController {
 	@PostMapping(value = "/save")
 	public ResponseEntity<InstrutorDTO> create(@RequestBody InstrutorDTO dto) {
 		Instrutor entity = service.create(dto);
+		validaPorCpfEEmail(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(entity.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	private void validaPorCpfEEmail(InstrutorDTO dto) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(dto.getCpf());
+		if (obj.isPresent() && obj.get().getId() != dto.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+		}
+
+		obj = pessoaRepository.findByEmail(dto.getEmail());
+		if (obj.isPresent() && obj.get().getId() != dto.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema");
+		}
+		
 	}
 	
 }
--- helpdesk/src/main/java/com/luan/helpdesk/controller/exceptions/ControllerExceptionHandler.java
+++ helpdesk/src/main/java/com/luan/helpdesk/controller/exceptions/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.luan.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.luan.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -23,6 +24,19 @@ public class ControllerExceptionHandler {
 												);
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandartError> DataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+		
+		StandartError error = new StandartError(System.currentTimeMillis(),
+												HttpStatus.BAD_REQUEST.value(),
+												"Data violation",
+												ex.getMessage(),
+												request.getRequestURI()
+												);
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 	
 }
