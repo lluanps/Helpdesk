4866d60bcd02b6ba25b20465e6f2d1c3442fd3f5
lluanps <lluanps@gmail.com>
2023-06-25 15:14:12 -0300

    Valida se CPF ou EMAIL ja existem no banco antes de cadastrar Cliente e retona msg de erro

--- helpdesk/src/main/java/com/luan/helpdesk/services/ClienteService.java
+++ helpdesk/src/main/java/com/luan/helpdesk/services/ClienteService.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luan.helpdesk.domain.Cliente;
+import com.luan.helpdesk.domain.Pessoa;
 import com.luan.helpdesk.domain.dtos.ClienteDTO;
 import com.luan.helpdesk.repositories.ClienteRepository;
+import com.luan.helpdesk.repositories.PessoaRepository;
+import com.luan.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.luan.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +19,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
@@ -27,8 +33,23 @@ public class ClienteService {
 	}
 
 	public Cliente create(ClienteDTO dto) {
+		dto.setId(null);
+		validaPorCpfEEmail(dto);
 		Cliente save = new Cliente();
 		return repository.save(save);
+	}
+
+	private void validaPorCpfEEmail(ClienteDTO dto) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(dto.getCpf());
+		if (obj.isPresent() && obj.get().getId() != dto.getId()) {
+			throw new DataIntegrityViolationException("CPF ja cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(dto.getEmail());
+		if (obj.isPresent() && obj.get().getId() != dto.getId()) {
+			throw new DataIntegrityViolationException("Email ja cadastrado no sistema!");
+		}
+		
 	}
 
 	
